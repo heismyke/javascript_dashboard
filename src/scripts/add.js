3142444e01198d85f4011e12658037dd88ced428
myke <81974207+heismyke@users.noreply.github.com>
2023-09-18 12:26:47 +0100

    javascript dashboard

--- src/scripts/add.js
+++ src/scripts/add.js
@@ -1,9 +1,15 @@
 export const addUserButton = document.getElementById('addBtn');
-        const addUI = document.getElementById('addUI');
-
+ const addUI = document.getElementById('addUI');
+const addUserUi = document.getElementById('addUserUi');
 var isAddUserButtonClicked = false;
 
 export const add = () => {
     isAddUserButtonClicked = true;
     addUI.style.display = 'flex';
+    addUI.classList.add('addUI');
 }
+
+addUI.addEventListener('click', function (e) {
+    console.log(e.target)
+   
+})
--- src/scripts/del.js
+++ src/scripts/del.js
@@ -1,6 +1,8 @@
 export const deleteButton = document.getElementById('delBtn');
-
+const delUI = document.getElementById('delUI');
+var isDeleteButtonClicked = false;
 
 export const del = () => {
-    console.log(deleteButton)
+    isDeleteButtonClicked = true;
+    delUI.style.display = 'flex';
 }
