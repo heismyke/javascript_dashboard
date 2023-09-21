f10368488fa5807affe685cd9cdff43b91ac53db
myke <81974207+heismyke@users.noreply.github.com>
2023-09-21 12:42:08 +0100

    javascript dashboard

--- src/scripts/addUserForm.js
+++ src/scripts/addUserForm.js
@@ -1,3 +1,4 @@
+import { cancelBtn } from "./cancel.js";
 const addUserForm = document.getElementById('addUserForm');
 addUserForm.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -8,18 +9,62 @@ addUserForm.addEventListener('submit', function(event) {
     const phoneInput = event.target.querySelector("#number");
 
     const name = nameInput.value;
+    const email = emailInput.value;
+    const addresses = addressesInput.value;
+    const phone = phoneInput.value;
+
     if (!name) {
         const nameInputDiv = document.getElementById('nameInputDiv');
         const errorDiv = document.createElement('div');
         errorDiv.textContent = "Name is required!";
         errorDiv.classList.add('errorDiv')
+        setTimeout(() => {
+            errorDiv.remove();
+        }, 2000)
         nameInputDiv.appendChild(errorDiv);
         nameInput.addEventListener("keydown", function() {
             errorDiv.remove();
         });
+       cancelBtn.forEach((buttons) => {
+        buttons.addEventListener("click", function() {
+            errorDiv.remove();
+        });
+       })
     }
+    if(!email || isEmailValid(email)) {
+        const emailInputDiv = document.getElementById('emailInputDiv');
+        const errorDiv = document.createElement('div');
+        errorDiv.textContent = "Email is required!";
+        errorDiv.classList.add('errorDiv')
+        setTimeout(() => {
+            errorDiv.remove();
+        }, 2000)
+        emailInputDiv.appendChild(errorDiv);
+        emailInput.addEventListener("keydown", function() {
+            errorDiv.remove();
+        });
+        cancelBtn.forEach((buttons) => {
+            buttons.addEventListener("click", function() {
+                errorDiv.remove();
+            });
+        })
+    } 
+    
     
 
 });
 
+function isEmailValid(email) {
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    if (emailPattern.test(email)) {
+        
+        return true;
+    } else {
+       
+        alert("Please enter a valid email");
+        return false;
+    }
+
+    
+}
 
